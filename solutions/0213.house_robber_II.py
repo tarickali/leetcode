1ed70c5798a04579b42ca36e28ec5cee48aa977f
tarickali <tarickali97@gmail.com>
2022-11-25 06:36:15 +0200

    Solved 213. House Robber II

---
+++ solutions/0213.house_robber_II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        def walk(i: int, j: int) -> int:
+            a, b, c = 0, nums[i], nums[i]
+            for i in range(i+1, j):
+                c = max(nums[i] + a, b)
+                a, b = b, c
+            return c
+        if len(nums) == 1: return nums[0]
+        return max(walk(0, len(nums) - 1), walk(1, len(nums)))
